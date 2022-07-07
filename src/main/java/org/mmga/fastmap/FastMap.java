20adfca027ea8ce5ab95ebf7b0be8ed29a20b49d
wzp <55318422+wzp-2008@users.noreply.github.com>
2022-07-07 19:55:26 +0800

    Initial commit

---
+++ src/main/java/org/mmga/fastmap/FastMap.java
@@ -0,0 +1,23 @@
+package org.mmga.fastmap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created On 2022/7/7 19:52
+ *
+ * @author wzp
+ * @version 1.0.0
+ */
+public final class FastMap extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
